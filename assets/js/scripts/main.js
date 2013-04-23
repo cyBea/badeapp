92291be243074100bb04149b15637d74b84440dc
Stefan <Stefan@allproperty.com.sg>
2013-04-23 19:12:22 +0200

    Repo weiter aufgeräumt

---
+++ assets/js/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    initMap();
+});
+
+function initMap() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(map);  
+}
