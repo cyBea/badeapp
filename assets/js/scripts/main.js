ffec2db5fb8952db49f91e742978dc5b9244979e
Nikolas Ortlieb <s0530594@htw-berlin.de>
2013-05-28 16:35:46 +0200

    #map height automatically set onLoad and onResize

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
     initMap();
+    setMapDimensions();
 });
+
+$(window).resize(function() {
+	setMapDimensions();
+});
+
 
 function initMap() {
     badestellenMap = badestellenMap();
+}
+function setMapDimensions() {
+	$('#map').css('height', $(window).height()-140)
 }
