5459cdbbdeb2626fce2c33be0dc82101d074a0ee
Stefan <Stefan@allproperty.com.sg>
2013-06-14 12:24:08 +0200

    Zoom auf Badestelle gefixed, Javascript strukturiert

--- assets/js/scripts/map.js
+++ assets/js/scripts/map.js
@@ -19,3 +19,27 @@ function initMap() {
 
     return map;
 }
+
+function setMapDimensions() {
+    $('#map').css('height', $(window).height() - $('#header').outerHeight(true));
+}
+
+// erwarten Koordinaten in Form eines Strings example: "4.53;13.24"
+function zoomToBathplace(coordString) {
+    var coords = coordString.split(";");
+    var LatLng = new L.LatLng(parseFloat(coords[0]), parseFloat(coords[1]));
+    map.panTo(LatLng);
+    map.setZoom(14);
+}
+
+// erwarten Boundingbox(Punkt oben links und Punkt unten rechts) in Form eines Strings example: "LatLng(52.44817, 13.57023);LatLng(52.37509, 13.65709)"
+function zoomToLake(coordstring) {
+    var coords = coordstring.split(";");
+    var p1 = coords[0].split(",");
+    var p2 = coords[1].split(",");
+    var bounds = new L.LatLngBounds(
+        new L.LatLng(parseFloat(p1[0].substring(7)), parseFloat(p1[1])),
+        new L.LatLng(parseFloat(p2[0].substring(7)), parseFloat(p2[1]))
+    );
+    map.fitBounds(bounds);
+}
--- assets/js/scripts/navi.js
+++ assets/js/scripts/navi.js
@@ -20,16 +20,7 @@ $(function() {
             duration : 'medium', 
             easing : 'linear'
         });    
-        var coords = $('#see').val().split(";");
-        var p1 = coords[0].split(",");
-        var p2 = coords[1].split(",");
-        var bounds = new L.LatLngBounds(
-            new L.LatLng(parseFloat(p1[0].substring(7)), parseFloat(p1[1])),
-            new L.LatLng(parseFloat(p2[0].substring(7)), parseFloat(p2[1]))
-        );
-        map.fitBounds(bounds);
-        
-        return false;
+        zoomToLake($('#see').val());
     });
 
     $('#badestelle').change(function(e) {
@@ -37,11 +28,6 @@ $(function() {
             duration : 'medium', 
             easing : 'linear'
         });
-        var coords = $('#badestelle').val().split(";");
-        var LatLng = new L.LatLng(parseFloat(coords[0]), parseFloat(coords[1]));
-        map.panTo(LatLng);
-        map.zoomIn(3);
-
-        return false;
+        zoomToBathplace($('#badestelle').val());
     });
 }); 
