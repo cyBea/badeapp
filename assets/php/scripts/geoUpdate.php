9d95a86d9417e8a9e128703110e26b74a592f935
cyBerta <richy@cyborgsociety.org>
2013-06-24 15:27:54 +0200

    markers long and lat via geoUpdate.php updated

---
+++ assets/php/scripts/geoUpdate.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php -q
+<?php
+$file = "../../../data/lakes_with_markers.json";
+
+$geofile =  "../../../data/geoBadestellen.json";
+
+$jsonObject = json_decode(file_get_contents($file), true);
+$geoJSON = json_decode(file_get_contents($geofile), true);
+
+
+foreach ($jsonObject as $gewaesser => $badestelle) {
+
+	foreach ($badestelle["markers"] as $badestellenname => $badevalues){
+	$jsonObject[$gewaesser]["markers"][$badestellenname]["coordinates"][0] = floatval($geoJSON[$badestellenname]["lat"]);
+	$jsonObject[$gewaesser]["markers"][$badestellenname]["coordinates"][1] = floatval($geoJSON[$badestellenname]["long"]);
+	
+	
+	}
+	
+	
+}
+
+$jsonObject2 = json_encode($jsonObject, JSON_UNESCAPED_UNICODE);
+
+$outfile = "../../../data/lakes_with_markers2.json";
+file_put_contents($outfile, $jsonObject2);
+
+
+?>
